2b81e84640979e120002224bf8c87b89115e7530
Chiba-Karajos Anca Petruta <anca.petra2003@gmail.com>
2018-08-08 11:44:31 +0300

    Adding Service Worker for cache

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: '<pk.eyJ1IjoiYW5jYXBldHJhIiwiYSI6ImNqam55N2tzZzVicmUzd256cnY0NXE1MHcifQ.Xjs1rdP5nCpPVHFoXVkYCQ>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
