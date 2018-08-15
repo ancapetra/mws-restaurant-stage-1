1fa84b06b36c99a9ee5f313b338daa777d59152b
Chiba-Karajos Anca Petruta <anca.petra2003@gmail.com>
2018-08-15 08:42:02 +0300

    Made it responsive. Added ARIA roles. Service worker is working.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<pk.eyJ1IjoiYW5jYXBldHJhIiwiYSI6ImNqam55N2tzZzVicmUzd256cnY0NXE1MHcifQ.Xjs1rdP5nCpPVHFoXVkYCQ>',
+    mapboxToken: 'pk.eyJ1IjoiYW5jYXBldHJhIiwiYSI6ImNqa25vZmNwMzIzZzEzdnAwbWZqYmEyZzAifQ.563JR9mZB2ArelGWqa2Uuw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -88,18 +88,18 @@ initMap = () => {
 
   updateRestaurants();
 }
-/* window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-} */
+//  window.initMap = () => {
+//   let loc = {
+//     lat: 40.722216,
+//     lng: -73.987501
+//   };
+//   self.map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 12,
+//     center: loc,
+//     scrollwheel: false
+//   });
+//   updateRestaurants();
+// } 
 
 /**
  * Update page and map for current restaurants.
@@ -208,4 +208,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
